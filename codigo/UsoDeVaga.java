c5f20390b62a7ec64f07b871fa870421fefb35b9
Pwyll <129997695+Pwyll38@users.noreply.github.com>
2023-10-04 12:10:37 -0300

    Update UsoDeVaga.java

--- codigo/UsoDeVaga.java
+++ codigo/UsoDeVaga.java
@@ -1,4 +1,6 @@
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class UsoDeVaga {
 
@@ -10,16 +12,36 @@ public class UsoDeVaga {
 	private LocalDateTime saida;
 	private double valorPago;
 
-	public UsoDeVaga(Vaga vaga) {
+	public UsoDeVaga(Vaga vaga, LocalDateTime entrada, LocalDateTime saida, double valorPago) {
+		this.vaga = vaga;
+		this.entrada = entrada;
+		this.saida = saida;
+		this.valorPago = valorPago;
 		
 	}
 
-	public double sair() {
+	public double sair() {//Não entendi o que esse método é para fazer
+		double valor = this.valorPago();
+
+		this.vaga.sair();
+
+		return valor;
+
+
 		
 	}
 
 	public double valorPago() {
-		
+		long periodo = entrada.until(saida, ChronoUnit.MINUTES);
+		int minutos = (int)periodo/15;
+		double valor = (double)(minutos * VALOR_FRACAO);
+
+		if(valor>VALOR_MAXIMO){
+			valor = VALOR_MAXIMO;
+		}
+
+		return valor;
+
 	}
 
 }
