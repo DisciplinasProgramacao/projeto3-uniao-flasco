7b3e4877bb31c3177a62c404f94ecd00915d7722
Henry <89222000+HenrySven@users.noreply.github.com>
2023-10-16 14:43:57 -0300

    Update Cliente.java
    serialização da classe Cliente

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,7 +1,10 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente {
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private String nome;
 	private String id;
