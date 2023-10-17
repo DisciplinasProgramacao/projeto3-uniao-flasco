54c45e6890c9776b6b49d56dbdd98dfde6ef0a9b
Renato Matos <renatobusiness@gmail.com>
2023-10-17 19:39:04 -0300

    Upadate Classes veiculo e VeiculoTest #3 #11 #24

--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -1,71 +1,159 @@
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+/**
+ * A classe Veiculo representa um veículo com uma placa associada e um histórico
+ * de usos de vagas. Cada veículo pertence a um cliente proprietário e pode estacionar
+ * em diferentes vagas durante o tempo.
+ * 
+ * @author @RenatoMAP77
+ */
+public class Veiculo implements Serializable {
+    private String placa;
+    private List<UsoDeVaga> usos;
+    private Cliente proprietario;
 
-public class Veiculo {
+    /**
+     * Construtor da classe Veiculo. Inicializa uma instância de veículo com a placa fornecida.
+     *
+     * @param placa A placa do veículo.
+     */
+    public Veiculo(String placa) {
+        setPlaca(placa);
+    }
 
-	private String placa;
-	private UsoDeVaga[] usos;
+    /**
+     * Define o cliente proprietário do veículo.
+     *
+     * @param proprietario O cliente proprietário do veículo.
+     */
+    public void setProprietario(Cliente proprietario) {
+        this.proprietario = proprietario;
+    }
 
-	
+    /**
+     * Obtém o cliente proprietário do veículo.
+     *
+     * @return O cliente proprietário do veículo.
+     */
+    public Cliente getProprietario() {
+        return proprietario;
+    }
 
-	public String getPlaca() {
-		return placa;
-	}
+    /**
+     * Define a placa do veículo.
+     *
+     * @param placa A placa do veículo.
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
 
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
+    /**
+     * Obtém a placa do veículo.
+     *
+     * @return A placa do veículo.
+     */
+    public String getPlaca() {
+        return placa;
+    }
 
-	public UsoDeVaga[] getUsos() {
-		return usos;
-	}
+    /**
+     * Define a lista de usos de vagas associada ao veículo.
+     *
+     * @param usos A lista de usos de vagas do veículo.
+     */
+    public void setUsos(List<UsoDeVaga> usos) {
+        this.usos = usos;
+    }
 
-	public void setUsos(UsoDeVaga[] usos) {
-		this.usos = usos;
-	}
+    /**
+     * Obtém a lista de usos de vagas associada ao veículo.
+     *
+     * @return A lista de usos de vagas do veículo.
+     */
+    public List<UsoDeVaga> getUsos() {
+        return usos;
+    }
 
-	public Veiculo(String placa) {
-		setPlaca(placa);
-	}
+    /**
+     * Estaciona o veículo em uma vaga específica, registrando o uso da vaga e marcando-a como indisponível.
+     *
+     * @param vaga A vaga em que o veículo será estacionado.
+     * @throws IllegalArgumentException Se a vaga não existir ou não estiver disponível.
+     */
+    public void estacionar(Vaga vaga) {
+        if (vaga == null || !vaga.isDisponivel())
+            throw new IllegalArgumentException("A VAGA NÃO EXISTE OU NÃO ESTÁ DISPONÍVEL");
 
-	public void estacionar(Vaga vaga) {
-				if (vaga == null || !vaga.isDisponivel()) throw new IllegalArgumentException("A VAGA NÃO EXISTE OU NÃO ESTÁ DISPONÍVEL");
-				if (vaga.isDisponivel()) {
-					vaga.setDisponivel(false);
-					UsoDeVaga uso = new UsoDeVaga(vaga);
-					uso.setEntrada(System.currentTimeMillis());
-					uso.setVeiculo(this);
-					uso.setVaga(vaga);
-					uso.setDisponivel(false);
-					uso.setSaida(0);
-					uso.setValor(0);
-					this.usos.add(uso);
-		
-	}
-	
-}
+        if (vaga.isDisponivel()) {
+            vaga.setDisponivel(false);
+            LocalDateTime entrada = LocalDateTime.now();
+            UsoDeVaga uso = new UsoDeVaga(vaga, entrada);
+            usos.add(uso);
+        }
+    }
 
-	public double sair() {
-		double sair = 0.0;
-		//ver custo hora depois
-		return sair;
-		
-	}
+    /**
+     * Registra a saída do veículo de uma vaga específica e calcula o valor a ser pago.
+     *
+     * @param vaga A vaga da qual o veículo está saindo.
+     * @return O valor a ser pago pelo uso da vaga.
+     */
+    public double sair(Vaga vaga) {
+        double valor = 0.0;
 
-	public double totalArrecadado() {
-		double totalArrecadado = 0.0;
-		return totalArrecadado;
-	}
+        for (UsoDeVaga usoDeVaga : usos) {
+            if (vaga.equals(usoDeVaga.getVaga())) {
+                LocalDateTime saida = LocalDateTime.now();
+                valor = usoDeVaga.sair(saida);
+                break;
+            }
+        }
 
-	public double arrecadadoNoMes(int mes) {
-		double arrecadadoNoMes = 0.0;
-		return arrecadadoNoMes;
-		
-	}
+        return valor;
+    }
 
-	public int totalDeUsos() {
-		int totalDeUsos = 0;
-		return totalDeUsos;
-		
-	}
+    /**
+     * Calcula o total arrecadado com os usos de vagas feitos pelo veículo.
+     *
+     * @return O total arrecadado com os usos de vagas.
+     */
+    public double totalArrecadado() {
+        double totalArrecadado = 0.0;
+        for (UsoDeVaga usoDeVaga : usos) {
+            totalArrecadado += usoDeVaga.getValorPago();
+        }
+        return totalArrecadado;
+    }
 
+    /**
+     * Calcula o total arrecadado com os usos de vagas feitos pelo veículo em um mês específico.
+     *
+     * @param mes O mês para o qual se deseja calcular a arrecadação (1 para janeiro, 2 para fevereiro, etc.).
+     * @return O total arrecadado no mês especificado.
+     */
+    public double arrecadadoNoMes(int mes) {
+        double arrecadadoNoMes = 0.0;
+        for (UsoDeVaga usoDeVaga : usos) {
+            LocalDateTime entrada = usoDeVaga.getEntrada();
+            Month monthOfYear = Month.of(entrada.getMonthValue());
+            int mesEntrada = monthOfYear.getValue();
+            if (mesEntrada == mes) {
+                arrecadadoNoMes += usoDeVaga.getValorPago();
+            }
+        }
+        return arrecadadoNoMes;
+    }
+
+    /**
+     * Obtém o número total de usos de vagas realizados pelo veículo.
+     *
+     * @return O número total de usos de vagas.
+     */
+    public int totalDeUsos() {
+        return usos.size();
+    }
 }
