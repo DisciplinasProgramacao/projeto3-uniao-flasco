4e0c64c6a3e9707813a358625367f317117d26e4
mariaeduardagsf <129904639+mariaeduardagsf@users.noreply.github.com>
2023-10-18 11:02:27 -0300

    Update Vaga.java
    Serialização da Classe Vaga

--- codigo/Vaga.java
+++ codigo/Vaga.java
@@ -1,24 +1,55 @@
 
 
-public class Vaga {
+public class Vaga implements Serializable {
 
-	private String id;
-	private boolean disponivel;
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private boolean disponivel;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
 
 	public Vaga(int fila, int numero) {
-		
+		int valor = fila + numero;
+		setId("" + valor);
 	}
 
 	public boolean estacionar() {
-		
+		if(disponivel() == true)
+		{
+			return true;
+		}
+		else return false;
 	}
 
 	public boolean sair() {
-		
+		if(disponivel()== true)
+		{
+			return false;
+		}
+		else return true;
 	}
 
 	public boolean disponivel() {
-		
+		if(isDisponivel() == true)
+		{
+			return true;
+		}
+		else return false;
 	}
 
 }
