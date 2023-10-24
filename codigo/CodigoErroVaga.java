f9cbb07c3b323a0a49c5bc1d37781c187a4b3582
Henry <89222000+HenrySven@users.noreply.github.com>
2023-10-24 20:34:26 -0300

    Create CodigoErroVaga.java

---
+++ codigo/CodigoErroVaga.java
@@ -0,0 +1,19 @@
+public enum CodigoErroVaga implements CodigoErro {
+    VAGA_NAO_FINALIZADA(401) {
+        @Override
+        public int getCodigo() {
+            return 1;
+        }
+    };
+
+    private final int numero;
+
+    private CodigoErroVaga(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public int getNumero() {
+        return numero;
+    }
+}
