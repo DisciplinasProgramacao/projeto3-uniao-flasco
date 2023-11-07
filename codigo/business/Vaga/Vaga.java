47ffb4b8d8f4fac523faf4ae8ee11a087c0a5933
RenatoMAP77 <renatomapbusiness@gmail.com>
2023-11-07 16:17:34 -0300

    Correção Vaga

--- codigo/business/Vaga/Vaga.java
+++ codigo/business/Vaga/Vaga.java
@@ -8,7 +8,7 @@ public class Vaga implements Serializable {
 
     private String id;
     private boolean disponivel;
-    private String veiculoEstacionado;
+    
 
     public enum LetraFila {
         A(1), B(2), C(3), D(4), E(5), F(6), G(7), H(8), I(9), J(10), K(11), L(12), M(13), N(14), O(15), P(16), Q(17),
@@ -62,18 +62,11 @@ public class Vaga implements Serializable {
         this.disponivel = true; // Por padrão a vaga está disponível quando criada
     }
 
-    public String getVeiculoEstacionado() {
-        return veiculoEstacionado;
-    }
-
-    public void setVeiculoEstacionado(String veiculoEstacionado) {
-        this.veiculoEstacionado = veiculoEstacionado;
-    }
 
     public boolean estacionar(String placa) {
         if (isDisponivel()) {
             setDisponivel(false);
-            setVeiculoEstacionado(placa);
+            
             return true;
         }
         return false;
@@ -82,7 +75,7 @@ public class Vaga implements Serializable {
     public boolean sair() {
         if (!isDisponivel()) {
             setDisponivel(true);
-            setVeiculoEstacionado(null);
+            
             return true;
         }
         return false;
