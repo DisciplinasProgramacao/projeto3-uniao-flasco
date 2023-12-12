f02b9bc5e95e96baa0edd45e587cd87069b2efba
Pwyll <129997695+Pwyll38@users.noreply.github.com>
2023-12-12 11:32:36 -0300

    Update ClienteTest.java

--- codigo/teste/ClienteTest.java
+++ codigo/teste/ClienteTest.java
@@ -1,11 +1,16 @@
 package teste;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 import business.Cliente.Cliente;
 import business.Plano.Plano;
 import business.Veiculo.Veiculo;
+import business.UsoDeVaga.UsoDeVaga;
 
 public class ClienteTest {
 
@@ -44,4 +49,28 @@ public class ClienteTest {
         assertTrue(cliente.possuiVeiculo("ABC1234"));
         assertFalse(cliente.possuiVeiculo("XYZ5678"));
     }
+
+    @Test
+    void testGetTotalUsos() {
+        int totalUsos = cliente.getTotalUsos();
+        assertEquals(0,totalUsos);
+
+        List<UsoDeVaga> usos = new ArrayList<UsoDeVaga>();
+        UsoDeVaga uso = new UsoDeVaga(null, null);
+        usos.add(uso);
+        veiculo.setUsos(usos);
+        assertEquals(1,totalUsos);
+    }
+
+    @Test
+    void testGetValorArrecadado(){
+        List<UsoDeVaga> usos = new ArrayList<UsoDeVaga>();
+        UsoDeVaga uso = new UsoDeVaga(null, null);
+        usos.add(uso);
+        veiculo.setUsos(usos);
+
+        assertEquals(0, cliente.getValorArrecadado(null));
+        assertEquals(0, cliente.getValorArrecadado("ABC1234"));
+    }
 }
+
