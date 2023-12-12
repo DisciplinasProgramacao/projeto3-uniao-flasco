ba3be2ff684af7c97f4138c96d8929b16090f3d9
Pwyll <129997695+Pwyll38@users.noreply.github.com>
2023-12-12 11:32:24 -0300

    Update EstacionamentoTest.java

--- codigo/teste/EstacionamentoTest.java
+++ codigo/teste/EstacionamentoTest.java
@@ -1,12 +1,19 @@
 package teste;
 
 import business.Plano.Horista;
+import business.Plano.Mensalista;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+
 import business.Estacionamento.Estacionamento;
 import business.Cliente.Cliente;
 import business.Plano.Plano;
+import business.Veiculo.Veiculo;
+import business.Plano.Mensalista;;
 
 class EstacionamentoTest {
 
@@ -17,5 +24,42 @@ class EstacionamentoTest {
         estacionamento = new Estacionamento("Estacionamento Teste", 5, 10, 1);
     }
 
-    /* @todo */
+    @Test
+    void testAddVeiculo(){
+        Veiculo veic = new Veiculo("ABC1234");
+        Cliente cliente = new Cliente("Teste", "1", null);
+        estacionamento.addCliente(cliente);
+
+        estacionamento.addVeiculo(veic, "1");
+
+        assertEquals(cliente.getVeiculos().size(), 1);
+    }
+
+    @Test 
+    void testEstacionar(){
+        Veiculo veiculo = new Veiculo("ABC1234");
+        estacionamento.estacionar(veiculo);
+        assertTrue(veiculo.isEstacionado());
+    }
+
+    @Test
+    void sair(){
+        Veiculo veiculo = new Veiculo("ABC1234");
+        Mensalista mensalista = new Mensalista("Mensalista");
+        estacionamento.estacionar(veiculo);
+        
+        assertEquals(estacionamento.sair(veiculo, mensalista),0);
+    }
+    @Test
+    void testTotalArrecadado(){
+        Cliente cliente = new Cliente(null, null, null);
+        estacionamento.addCliente(cliente);
+        assertEquals(estacionamento.totalArrecadado(),0);
+    }
+    @Test
+    void testArrecadadoNoMes(){
+        Cliente cliente = new Cliente(null, null, null);
+        estacionamento.addCliente(cliente);
+        assertEquals(estacionamento.arrecadadoNoMes(1),0);
+    }
 }
